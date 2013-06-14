7fef8f71a40b276f2a16513990f435e05d170d6e
Hector Acosta <hector.acosta@gmail.com>
2013-06-14 17:55:08 -0500

    Correctly change headers
    Signed-off-by: Hector Acosta <hector.acosta@gmail.com>

--- tonto.py
+++ tonto.py
@@ -4,7 +4,8 @@ import re
 import argparse
 import itertools
 import logging
-import bs4
+import urllib.request
+import lxml.html
 import irc.bot
 import irc.client
 import random
@@ -21,13 +22,14 @@ class TontoBot(irc.bot.SingleServerIRCBot):
 	def __init__(self, serverspec, channel, nickname, realname, seen_urlpath='./seenurls.pickle'):
 		irc.bot.SingleServerIRCBot.__init__(self, [serverspec], nickname, realname)
 		self.channel = channel
+		self.seen_urlpath = seen_urlpath
 		logging.info("channel= %s" % channel)
 		try:
 			with open(seen_urlpath, 'rb') as f:
 				self.urlhist = pickle.load(f)
 		except:
 			self.urlhist = {}
-		self.urlfd = 
+		atexit.register(self._dumphist)
 
 	def on_welcome(self, connection, event):
 		logging.info("joining %s", self.channel)
@@ -39,22 +41,26 @@ class TontoBot(irc.bot.SingleServerIRCBot):
 		except:
 			pass
 
+	def _dumphist(self):
+		try:
+			with open(self.seen_urlpath, 'wb') as f:
+				pickle.dump(self.urlhist, f)
+		except:
+			logging.exception("Oh noes, history is lost")
 
 	def on_pubmsg(self, connection, event):
 		line = event.arguments[0]
-		opener = urllib.request.build_opener()
-		opener.add_headers = [('User-agent', 'Mozilla/5.0')]
 		for u in get_urls(line):
 			try:
-				fd = opener.open(u)
-				soup = bs4.BeautifulSoup(fd.read(self.FETCH_MAX))
-				title = soup.title.string
-				if title in self.urlhist:
+				req = urllib.request.Request(u, headers={'User-agent': 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11'})
+				fd = urllib.request.urlopen(req)
+				root = lxml.html.fromstring(fd.read(self.FETCH_MAX))
+				title = root.find('.//title').text
+				if u in self.urlhist:
 					connection.privmsg(self.channel, "[repost] " + title)
 				else:
 					connection.privmsg(self.channel, title)
-				with open(
-				picke.dump(self.urlhist):
+					self.urlhist[u] = title
 			except:
 				logging.exception("Failed with: %s" % line)
 				self._best_effort_send(connection, random.choice(self.FAIL_MSGS))
@@ -63,7 +69,7 @@ def get_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--server', default='irc.freenode.net')
 	parser.add_argument('--nickname', default='tontobot')
-	parser.add_argument('--channel', default='#gultec2')
+	parser.add_argument('--channel', default='#gultec')
 	parser.add_argument('--realname', default='Tonto Bot')
 	parser.add_argument('-p', '--port', default=6667, type=int)
 	return parser.parse_args()
